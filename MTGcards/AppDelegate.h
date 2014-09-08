4c8af6968343cb971f81ca21cb58084667b72d9e
Connor Tinsley <connortinsley@yahoo.com>
2014-09-08 10:05:08 -0700

    Initial Commit

---
+++ MTGcards/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MTGcards
+//
+//  Created by CCT on 9/8/14.
+//  Copyright (c) 2014 CONNOR TINSLEY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
